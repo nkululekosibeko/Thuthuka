1627c6ea4d7d70156a64eb8240196bccfe4467ad
Nkululeko Sibeko <112828662+nkululekosibeko@users.noreply.github.com>
2024-09-09 17:06:04 +0200

    Add project files.

---
+++ Thuthuka Construction/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Thuthuka_Construction.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
